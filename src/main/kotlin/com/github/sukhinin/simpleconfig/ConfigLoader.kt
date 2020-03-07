440d8e7dbe6ddacfee2cd121c2cab989ddd54f7e
Boris Sukhinin <boris.sukhinin@corp.mail.ru>
2020-03-07 04:52:31 +0300

    feat: extend configuration loading support

--- src/main/kotlin/com/github/sukhinin/simpleconfig/ConfigLoader.kt
+++ src/main/kotlin/com/github/sukhinin/simpleconfig/ConfigLoader.kt
@@ -2,6 +2,7 @@ package com.github.sukhinin.simpleconfig
 
 import java.nio.file.Files
 import java.nio.file.Path
+import java.nio.file.Paths
 import java.nio.file.StandardOpenOption
 import java.util.*
 
@@ -9,11 +10,17 @@ object ConfigLoader {
 
     @JvmStatic
     fun getConfigFromSystemProperties(prefix: String): Config {
+        val prefixAndDot = if (prefix.endsWith('.')) prefix else "$prefix."
         return System.getProperties().stringPropertyNames()
-            .filter { name -> name.startsWith(prefix) }
+            .filter { name -> name.startsWith(prefixAndDot) }
             .associateWith { name -> System.getProperties().getProperty(name) }
-            .mapKeys { (name, _) -> name.removePrefix(prefix) }
+            .mapKeys { (name, _) -> name.removePrefix(prefixAndDot) }
             .let(::MapConfig)
+    }
+
+    @JvmStatic
+    fun getConfigFromPath(path: String): Config {
+        return getConfigFromPath(Paths.get(path))
     }
 
     @JvmStatic
@@ -25,7 +32,12 @@ object ConfigLoader {
 
     @JvmStatic
     fun getConfigFromSystemResource(name: String): Config {
-        return ClassLoader.getSystemResourceAsStream(name)
+        return getConfigFromResource(name, ClassLoader.getSystemClassLoader())
+    }
+
+    @JvmStatic
+    fun getConfigFromResource(name: String, classLoader: ClassLoader): Config {
+        return classLoader.getResourceAsStream(name)
             ?.use { stream -> Properties().apply { load(stream) } }
             ?.let { properties -> PropertiesConfig(properties) }
             ?: MapConfig(emptyMap())
