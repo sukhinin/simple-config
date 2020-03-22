64dc2eacf153f61467df5c441e2d333ff753e42d
Boris Sukhinin <boris.sukhinin@corp.mail.ru>
2020-03-22 21:19:56 +0300

    feat: add apikey to the list of masked config keys

--- src/main/kotlin/com/github/sukhinin/simpleconfig/MaskedConfig.kt
+++ src/main/kotlin/com/github/sukhinin/simpleconfig/MaskedConfig.kt
@@ -3,7 +3,7 @@ package com.github.sukhinin.simpleconfig
 class MaskedConfig(val config: Config, val keywords: Collection<String> = DEFAULT_KEYWORDS) : Config by config {
     companion object {
         @JvmStatic
-        val DEFAULT_KEYWORDS = listOf("passwd", "password", "secret")
+        val DEFAULT_KEYWORDS = listOf("apikey", "passwd", "password", "secret")
     }
 
     override fun get(key: String): String = if (keywords.any { key.contains(it) }) "******" else config.get(key)
